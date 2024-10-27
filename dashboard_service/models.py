0e50f9c17a3326ae9ce59d44ebcbb7326524eab3
Tom <abaotom14@gmail.com>
2024-10-27 13:20:34 +0800

    Add model

--- dashboard_service/models.py
+++ dashboard_service/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class BookingEvent(models.Model):
+    booking_id = models.IntegerField()
+    hotel_id = models.IntegerField()
+    room_id = models.CharField(max_length=60)
+    rpg_status = models.IntegerField()
+    night_of_stay = models.DateField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
--- data_provider/models.py
+++ data_provider/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Event(models.Model):
+    booking_id = models.IntegerField()
     hotel_id = models.IntegerField()
-    room_reservation_id = models.CharField(max_length=60)
-    room_id = models.IntegerField()
+    room_id = models.CharField(max_length=60)
     rpg_status = models.IntegerField()
     night_of_stay = models.DateField()
     timestamp = models.DateTimeField(auto_now_add=True)
